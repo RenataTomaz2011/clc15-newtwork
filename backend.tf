687ecb7e19a06a20a8b87b4b527e3465bb85e7f0
RenataTomaz2011 <renata.tomaz@aluno.faculdadeimpacta.com.br>
2026-05-10 21:20:21 +0000

    fix: corrige key do backend S3 def

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "clc15-renata-terraform"
-    key    = "network/terraform.tfstate"
+    key    = "newtwork/terraform.tfstate"
     region = "us-east-1"
   }
 }
