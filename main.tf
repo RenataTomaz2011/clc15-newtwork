dbbf1b7c5c5a86eb2937b5942382b9b8f87e64a2
RenataTomaz2011 <renata.tomaz@aluno.faculdadeimpacta.com.br>
2026-05-10 22:44:54 +0000

    feat: rede completa com NAT Gateways, EIPs e rotas privadas

--- main.tf
+++ main.tf
@@ -3,9 +3,10 @@ resource "aws_vpc" "terraform_vpc" {
   instance_tenancy     = "default"
   enable_dns_support   = true
   enable_dns_hostnames = true
-  tags = { Name = "clc15-tf-vpc" 
-           CC ="123456"
-           Owner ="Devops"
+  tags = {
+    Name  = "clc15-tf-vpc"
+    CC    = "123456"
+    Owner = "Devops"
   }
 }
 
@@ -44,6 +45,32 @@ resource "aws_internet_gateway" "tf_gw" {
   tags = { Name = "tf-vpc-igw" }
 }
 
+resource "aws_eip" "tf_ip_nat_1a" {
+  domain     = "vpc"
+  depends_on = [aws_internet_gateway.tf_gw]
+  tags = { Name = "tf-eip-nat-1a" }
+}
+
+resource "aws_eip" "tf_ip_nat_1b" {
+  domain     = "vpc"
+  depends_on = [aws_internet_gateway.tf_gw]
+  tags = { Name = "tf-eip-nat-1b" }
+}
+
+resource "aws_nat_gateway" "tf_natgateway_1a" {
+  allocation_id = aws_eip.tf_ip_nat_1a.id
+  subnet_id     = aws_subnet.subnet_public_1a.id
+  depends_on    = [aws_internet_gateway.tf_gw]
+  tags = { Name = "tf-natgateway-1a" }
+}
+
+resource "aws_nat_gateway" "tf_natgateway_1b" {
+  allocation_id = aws_eip.tf_ip_nat_1b.id
+  subnet_id     = aws_subnet.subnet_public_1b.id
+  depends_on    = [aws_internet_gateway.tf_gw]
+  tags = { Name = "tf-natgateway-1b" }
+}
+
 resource "aws_route_table" "tf_public_rt" {
   vpc_id = aws_vpc.terraform_vpc.id
   route {
@@ -65,7 +92,11 @@ resource "aws_route_table_association" "public_1b_association" {
 
 resource "aws_route_table" "tf_private_rt_1a" {
   vpc_id = aws_vpc.terraform_vpc.id
-  tags   = { Name = "tf-private-rt-1a" }
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.tf_natgateway_1a.id
+  }
+  tags = { Name = "tf-private-rt-1a" }
 }
 
 resource "aws_route_table_association" "private_1a_association" {
@@ -75,7 +106,11 @@ resource "aws_route_table_association" "private_1a_association" {
 
 resource "aws_route_table" "tf_private_rt_1b" {
   vpc_id = aws_vpc.terraform_vpc.id
-  tags   = { Name = "tf-private-rt-1b" }
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.tf_natgateway_1b.id
+  }
+  tags = { Name = "tf-private-rt-1b" }
 }
 
 resource "aws_route_table_association" "private_1b_association" {
@@ -84,13 +119,26 @@ resource "aws_route_table_association" "private_1b_association" {
 }
 
 output "vpc_id" {
-  value = aws_vpc.terraform_vpc.id
+  value       = aws_vpc.terraform_vpc.id
+  description = "ID da VPC"
 }
 
 output "public_subnets" {
-  value = [aws_subnet.subnet_public_1a.id, aws_subnet.subnet_public_1b.id]
+  value       = [aws_subnet.subnet_public_1a.id, aws_subnet.subnet_public_1b.id]
+  description = "IDs das subnets públicas"
 }
 
 output "private_subnets" {
-  value = [aws_subnet.subnet_private_1a.id, aws_subnet.subnet_private_1b.id]
+  value       = [aws_subnet.subnet_private_1a.id, aws_subnet.subnet_private_1b.id]
+  description = "IDs das subnets privadas"
+}
+
+output "nat_gateway_1a_public_ip" {
+  value       = aws_eip.tf_ip_nat_1a.public_ip
+  description = "IP público do NAT Gateway 1a"
+}
+
+output "nat_gateway_1b_public_ip" {
+  value       = aws_eip.tf_ip_nat_1b.public_ip
+  description = "IP público do NAT Gateway 1b"
 }
